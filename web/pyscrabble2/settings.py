04b52a183fa50c25f8c067362630caf317552c3c
Carlo Maria Massimo <cmassim@gmail.com>
2013-09-12 12:32:03 +0200

    added appserv to installed_apps

--- web/pyscrabble2/settings.py
+++ web/pyscrabble2/settings.py
@@ -135,7 +135,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'game'
+    'game',
+    'gunicorn'
 )
 
 # A sample logging configuration. The only tangible logging
